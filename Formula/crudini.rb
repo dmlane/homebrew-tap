757c2acb06db293b3f15a99e1ae855986f75ce70
David Lane <git2018@dmlane.net>
2023-11-11 04:12:13 +0100

    Latest

--- Formula/crudini.rb
+++ Formula/crudini.rb
@@ -4,7 +4,7 @@ class Crudini < Formula
 	homepage "https://github.com/pixelb/crudini"
 	url "https://github.com/pixelb/crudini/archive/refs/tags/0.9.5.tar.gz"
 	sha256 "b0492cb9ce126ce3c05ae7d8424c0fca5b26a788f593afe000150ba0c4b23cdb"
-	depends_on "python@3.12"
+	depends_on "python@3.11"
 	license "GPLv2"
 	def install
 		virtualenv_install_with_resources
