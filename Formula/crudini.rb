ad14b0e9e8e12d40bcdcfdd6aace58cef1ca6194
David Lane <git2018@dmlane.net>
2023-11-11 05:18:23 +0100

    Latest

--- Formula/crudini.rb
+++ Formula/crudini.rb
@@ -6,6 +6,7 @@ class Crudini < Formula
 	sha256 "b0492cb9ce126ce3c05ae7d8424c0fca5b26a788f593afe000150ba0c4b23cdb"
 	depends_on "python@3.11"
 	license "GPLv2"
+	version "0.9.5"
 	resource "iniparse" do
     	url "https://files.pythonhosted.org/packages/4c/9a/02beaf11fc9ea7829d3a9041536934cd03990e09c359724f99ee6bd2b41b/iniparse-0.5.tar.gz"
     	sha256 "932e5239d526e7acb504017bb707be67019ac428a6932368e6851691093aa842"
