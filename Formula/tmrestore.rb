a91ec3dacb261e2b1388d5f50aea3c8287bdd909
David Lane <git2018@dmlane.net>
2023-11-12 23:46:25 +0100

    Latest

--- Formula/tmrestore.rb
+++ Formula/tmrestore.rb
@@ -12,23 +12,11 @@ class Tmrestore < Formula
   depends_on "bash" 
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+  	inreplace "tmrestore.sh",/HB_ETC/,"#{etc}"
     bin.install "tmrestore.sh"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
   end
 
-  #test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test tmrestore`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-  #  system "false"
-  #end
+  test do
+    system "#{bin}/tmrestore --version"
+  end
 end
