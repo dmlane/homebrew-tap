7907ef5465ad338fb48acacfe2edc87d8064d6f9
David Lane <git2018@dmlane.net>
2023-11-14 02:40:12 +0100

    tm_restore V1.0.4

--- Formula/tmrestore.rb
+++ Formula/tmrestore.rb
@@ -1,15 +1,16 @@
 class Tmrestore < Formula
   desc "Restore time-machine backup from command-line"
   homepage "https://github.com/dmlane"
-  url "https://github.com/dmlane/tmrestore/archive/refs/tags/V1.0.3.tar.gz"
-  sha256 "1131c12190e2dac937bbc6c4267076d6ec007f3ffd560f03ed7647ff8195ae15"
+  url "https://github.com/dmlane/tmrestore/archive/refs/tags/V1.0.4.tar.gz"
+  sha256 "d200c353e59d58d9f4e0b1cf78fafeea64cc6b7f60e9958fa408727729f9eb7a"
   license "MIT"
 
   depends_on "bash"
 
   def install
-    inreplace "tmrestore", "HB_ETC", etc.to_s
     bin.install "tmrestore"
+	inreplace "common.env", "__VERSION__", version.to_s
+	prefix.install "common.env"
   end
 
   test do
