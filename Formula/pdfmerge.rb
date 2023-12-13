9863922cf68c3a8f4da4d9be7f4b9ee33c682bcf
David Lane <git2018@dmlane.net>
2023-12-13 12:13:08 +0000

    Updated pdfmerge.rb to release 2023.12.1035

--- Formula/pdfmerge.rb
+++ Formula/pdfmerge.rb
@@ -3,8 +3,8 @@ class Pdfmerge < Formula
 
   desc "Script to merge pdfs into a new pdf"
   homepage "https://github.com/dmlane/pdfmerge"
-  url "https://github.com/dmlane/pdfmerge/archive/refs/tags/2023.12.1034-rc0.tar.gz"
-  sha256 "d1d38c822d31365a98b9e2002a32c72beca4d34e5de09d4799337789a7969aed"
+  url "https://github.com/dmlane/pdfmerge/archive/refs/tags/2023.12.1035.tar.gz"
+  sha256 "c4998c78ba63c5f5316b3b2dc074ca7fbb580f4c401d17fbdec4723d43b03b98"
   license "MIT"
 
   depends_on "python@3.11"
