aed7e4aa7b12c11811c9f6545e104cddc91df66d
David Lane <git2018@dmlane.net>
2023-12-18 14:53:59 +0000

    Updated dmltools.rb to release 2023.12.1005-rc0

--- Formula/dmltools.rb
+++ Formula/dmltools.rb
@@ -3,8 +3,8 @@ class Dmltools < Formula
 
   desc "Collection of useful homegrown command-line tools"
   homepage "https://github.com/dmlane/dmltools"
-  url "https://github.com/dmlane/dmltools/archive/refs/tags/2023.12.1004-rc0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/dmlane/dmltools/archive/refs/tags/2023.12.1005-rc0.tar.gz"
+  sha256 "770d9ee8952ef6e9a41ddd0aeb5ff44af1762e844dceb88b60969ad8b7b61810"
   license "MIT"
 
   depends_on "python@3.11"
