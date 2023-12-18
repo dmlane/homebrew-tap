820d50ada5982d1146c12084c28f9de13e649820
David Lane <git2018@dmlane.net>
2023-12-18 20:45:40 +0000

    Updated dmltools.rb to release 2023.12.1006-rc0

--- Formula/dmltools.rb
+++ Formula/dmltools.rb
@@ -3,14 +3,23 @@ class Dmltools < Formula
 
   desc "Collection of useful homegrown command-line tools"
   homepage "https://github.com/dmlane/dmltools"
-  url "https://github.com/dmlane/dmltools/archive/refs/tags/2023.12.1005-rc0.tar.gz"
-  sha256 "770d9ee8952ef6e9a41ddd0aeb5ff44af1762e844dceb88b60969ad8b7b61810"
+  url "https://github.com/dmlane/dmltools/archive/refs/tags/2023.12.1006-rc0.tar.gz"
+  sha256 "a2114f51495a15dd0064c509ce6fef4da6e1727a94637e2b6ce11579b1f48c21"
   license "MIT"
 
   depends_on "python@3.11"
 
   def install
     virtualenv_install_with_resources
+  end
+
+  resource "eyed3" do
+    url "https://files.pythonhosted.org/packages/75/a5/263664ef1f1be58f72c8bc66ef128781af0a8110aeb591428d5c3a67b356/eyeD3-0.9.7.tar.gz"
+    sha256 "93b18e9393376a45114f9409d7cca119fb6f4f9a37d4b697b500af48b4c5cf0f"
+  end
+  resource "macos-tags" do
+    url "https://files.pythonhosted.org/packages/d4/6e/e0b2ea37ef831a5c6b5aebbd14701d96d9dc061f04a867b05335a4bc099d/macos-tags-1.5.1.tar.gz"
+    sha256 "f144c5bc05d01573966d8aca2483cb345b20b76a5b32e9967786e086a38712e7"
   end
 
   test do
