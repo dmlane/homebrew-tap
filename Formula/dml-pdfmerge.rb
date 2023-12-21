ded5baa16b482a2942ee3461740de00723df875a
David Lane <git2018@dmlane.net>
2023-12-21 11:07:10 +0000

    Updated dml-pdfmerge.rb to release 2023.12.1004

--- Formula/dml-pdfmerge.rb
+++ Formula/dml-pdfmerge.rb
@@ -1,23 +1,23 @@
 class DmlPdfmerge < Formula
   include Language::Python::Virtualenv
 
-  desc "Script to merge pdfs into a new pdf"
-  homepage "https://github.com/dmlane/pdfmerge"
-  url "https://github.com/dmlane/pdfmerge/archive/refs/tags/2023.12.1082-rc0.tar.gz"
-  sha256 "a29372e9a33c2200cd06208ebc81560d48a1356876df8b675c9e0f1de27867a4"
+  desc "A short description"
+  homepage "https://github.com/dmlane/dml-pdfmerge"
+  url "https://github.com/dmlane/dml-pdfmerge/archive/refs/tags/2023.12.1004.tar.gz"
+  sha256 "27dea9cbbc0019393d2f7b11e3b54b2a0df4f2068e15f195b95f2488b06265ed"
   license "MIT"
 
   depends_on "python@3.11"
 
-  resource "pypdf" do
-    url "https://files.pythonhosted.org/packages/89/d4/57ca6f20407a86e19f45b3e19441cf39aaabb4e7cb307f9a94b02d7dbedf/pypdf-3.17.2.tar.gz"
-    sha256 "d6f077060912f8292d7db3da04f7bf2428ac974781e11eef219193a22120f649"
-  end
-
   def install
     virtualenv_install_with_resources
   end
-
+  #---START-RESOURCES---
+  resource "pypdf" do
+    url "https://files.pythonhosted.org/packages/23/38/5e123ad1b071f6aa068d6c5a01ae384d1794fce93b42ad22da152232f878/pypdf-3.17.3.tar.gz"
+    sha256 "70c072218e3729218676bdf107e921fa49d1838c2f46056ce26d495c7e58f641"
+  end
+  #---END-RESOURCES---
   test do
     system "#{bin}/pdfmerge", "--version"
   end
