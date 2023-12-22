7030842f068a0c6b7d1eb3de643d1cf9a1b6a6c0
David Lane <git2018@dmlane.net>
2023-12-22 22:36:17 +0000

    Updated dml-check4duplicates.rb to release 2023.12.1002-rc0

--- Formula/dml-check4duplicates.rb
+++ Formula/dml-check4duplicates.rb
@@ -3,8 +3,8 @@ class DmlCheck4duplicates < Formula
 
   desc "Search for identical file in any of the supplied directories"
   homepage "https://github.com/dmlane/dml-check4duplicates"
-  url "https://github.com/dmlane/dml-check4duplicates/archive/refs/tags/2023.12.1001-rc0.tar.gz"
-  sha256 "5a73b40a3b2d22e98c32eff8c81367f9ccc72a58e9d7f6c4c71e4848b2121583"
+  url "https://github.com/dmlane/dml-check4duplicates/archive/refs/tags/2023.12.1002-rc0.tar.gz"
+  sha256 "3547fdfc9d77e4e32d7efca3dc71d197153d21cc14b1609851cf49503674a6ad"
   license "MIT"
 
   depends_on "python@3.11"
@@ -13,6 +13,26 @@ class DmlCheck4duplicates < Formula
     virtualenv_install_with_resources
   end
   #---START-RESOURCES---
+  resource "cffi" do
+    url "https://files.pythonhosted.org/packages/68/ce/95b0bae7968c65473e1298efb042e10cafc7bafc14d9e4f154008241c91d/cffi-1.16.0.tar.gz"
+    sha256 "bcb3ef43e58665bbda2fb198698fcae6776483e0c4a631aa5647806c25e02cc0"
+  end
+  resource "macos-tags" do
+    url "https://files.pythonhosted.org/packages/d4/6e/e0b2ea37ef831a5c6b5aebbd14701d96d9dc061f04a867b05335a4bc099d/macos-tags-1.5.1.tar.gz"
+    sha256 "f144c5bc05d01573966d8aca2483cb345b20b76a5b32e9967786e086a38712e7"
+  end
+  resource "mdfind-wrapper" do
+    url "https://files.pythonhosted.org/packages/0e/74/148968c2665c0f2db1fbd470fbb454b1f808ea5d4cb8d75bc99f451d0ece/mdfind-wrapper-0.1.5.tar.gz"
+    sha256 "c0dbd5bc99c6d1fb4678bfa1841a3380ccac61e9b43a26a8d658aa9cafe27441"
+  end
+  resource "pycparser" do
+    url "https://files.pythonhosted.org/packages/5e/0b/95d387f5f4433cb0f53ff7ad859bd2c6051051cebbb564f139a999ab46de/pycparser-2.21.tar.gz"
+    sha256 "e644fdec12f7872f86c58ff790da456218b10f863970249516d60a5eaca77206"
+  end
+  resource "xattr" do
+    url "https://files.pythonhosted.org/packages/91/ac/5898d1811abc88c3710317243168feff61ce12be220b9c92ee045ecd66c4/xattr-0.9.9.tar.gz"
+    sha256 "09cb7e1efb3aa1b4991d6be4eb25b73dc518b4fe894f0915f5b0dcede972f346"
+  end
   #---END-RESOURCES---
   test do
     system "#{bin}/check4duplicates", "--version"
