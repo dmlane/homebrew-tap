d06b65be53e7d1813891d908fb5520d4037f7426
David Lane <git2018@dmlane.net>
2024-01-30 10:52:04 +0100

    Updated dml-tools.rb to release 2024.1.1002

---
+++ Formula/dml-tools.rb
@@ -0,0 +1,20 @@
+class DmlTools < Formula
+  include Language::Python::Virtualenv
+
+  desc "Set of command-line tools which need python"
+  homepage "https://github.com/dmlane/dml-tools"
+  url "https://github.com/dmlane/dml-tools/archive/refs/tags/2024.1.1002.tar.gz"
+  sha256 "f3c8d75578924397f73e17d284d847408b9b5923487b1c54be5abfef24ffe9f9"
+  license "MIT"
+
+  depends_on "python@3.11"
+
+  def install
+    virtualenv_install_with_resources
+  end
+  #---START-RESOURCES---
+  #---END-RESOURCES---
+  test do
+    system "#{bin}/tools", "--version"
+  end
+end
