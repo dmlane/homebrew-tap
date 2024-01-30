ac6f45f5a116cad12b6115fcb24fefd46dc479d2
David Lane <git2018@dmlane.net>
2024-01-30 13:09:10 +0100

    Updated dml-tools.rb to release 2024.1.1019

---
+++ Formula/dml-tools.rb
@@ -0,0 +1,48 @@
+class DmlTools < Formula
+  include Language::Python::Virtualenv
+
+  desc "Set of command-line tools which need python"
+  homepage "https://github.com/dmlane/dml-tools"
+  url "https://github.com/dmlane/dml-tools/archive/refs/tags/2024.1.1019.tar.gz"
+  sha256 "1ddc3c14232772ed75a235e552c1f64aedfb18c422c47273fad7b8f052164965"
+  license "MIT"
+
+  depends_on "python@3.11"
+
+  def install
+    virtualenv_install_with_resources
+  end
+  #---START-RESOURCES---
+  resource "appdirs" do
+    url "https://files.pythonhosted.org/packages/d7/d8/05696357e0311f5b5c316d7b95f46c669dd9c15aaeecbb48c7d0aeb88c40/appdirs-1.4.4.tar.gz"
+    sha256 "7d5d0167b2b1ba821647616af46a749d1c653740dd0d2415100fe26e27afdf41"
+  end
+  resource "coverage" do
+    url "https://files.pythonhosted.org/packages/38/df/d5e67851e83948def768d7fb1a0fd373665b20f56ff63ed220c6cd16cb11/coverage-5.5.tar.gz"
+    sha256 "ebe78fe9a0e874362175b02371bdfbee64d8edc42a044253ddf4ee7d3c15212c"
+  end
+  resource "deprecation" do
+    url "https://files.pythonhosted.org/packages/5a/d3/8ae2869247df154b64c1884d7346d412fed0c49df84db635aab2d1c40e62/deprecation-2.1.0.tar.gz"
+    sha256 "72b3bde64e5d778694b0cf68178aed03d15e15477116add3fb773e581f9518ff"
+  end
+  resource "eyed3" do
+    url ""
+    sha256 ""
+  end
+  resource "filetype" do
+    url "https://files.pythonhosted.org/packages/bb/29/745f7d30d47fe0f251d3ad3dc2978a23141917661998763bebb6da007eb1/filetype-1.2.0.tar.gz"
+    sha256 "66b56cd6474bf41d8c54660347d37afcc3f7d1970648de365c102ef77548aadb"
+  end
+  resource "packaging" do
+    url "https://files.pythonhosted.org/packages/fb/2b/9b9c33ffed44ee921d0967086d653047286054117d584f1b1a7c22ceaf7b/packaging-23.2.tar.gz"
+    sha256 "048fb0e9405036518eaaf48a55953c750c11e1a1b68e0dd1a9d62ed0c092cfc5"
+  end
+  resource "toml" do
+    url "https://files.pythonhosted.org/packages/be/ba/1f744cdc819428fc6b5084ec34d9b30660f6f9daaf70eead706e3203ec3c/toml-0.10.2.tar.gz"
+    sha256 "b3bda1d108d5dd99f4a20d24d9c348e91c4db7ab1b749200bded2f839ccbe68f"
+  end
+  #---END-RESOURCES---
+  test do
+    system "#{bin}/tools", "--version"
+  end
+end
