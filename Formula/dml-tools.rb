b15b3482e917569ce6a2c2eae5d8b8c7c165bbb9
David Lane <git2018@dmlane.net>
2024-01-30 11:52:52 +0100

    Updated dml-tools.rb to release 2024.1.1011-rc0

--- Formula/dml-tools.rb
+++ Formula/dml-tools.rb
@@ -3,8 +3,8 @@ class DmlTools < Formula
 
   desc "Set of command-line tools which need python"
   homepage "https://github.com/dmlane/dml-tools"
-  url "https://github.com/dmlane/dml-tools/archive/refs/tags/2024.1.1010-rc0.tar.gz"
-  sha256 "e9ab61abcde14577fae6ac9a7ab6dd370209136445df59ccbd1707dc99788d14"
+  url "https://github.com/dmlane/dml-tools/archive/refs/tags/2024.1.1011-rc0.tar.gz"
+  sha256 "19ef2107e3d0ce4b67c12337864191f4fd45ccc303d41eb1ec80d75bd7f6e7c4"
   license "MIT"
 
   depends_on "python@3.11"
