1b88bd9e62200cacd741604b2b307616353930dc
David Lane <git2018@dmlane.net>
2025-05-05 18:23:17 +0100

    Latest

--- Formula/pdfclassify.rb
+++ Formula/pdfclassify.rb
@@ -1,5 +1,4 @@
 class Pdfclassify < Formula
-  include Language::Python::Virtualenv
   desc "Awesome CLI tool"
   homepage "https://github.com/dmlane/pdfclassify"
   url "https://github.com/dmlane/pdfclassify/releases/download/v1.0.22/pdfclassify-1.0.22.pyz"
@@ -10,7 +9,11 @@ class Pdfclassify < Formula
 
   def install
 	#virtualenv_install_with_resources using: Formula["python@3.12"].opt_bin/"python3"
-    virtualenv_install_with_resources
+	libexec.install "pdfclassify.pyz"
+	(bin/"pdfclassify").write <<~EOS
+		#!/bin/bash
+		exec #{Formula["python@3.12"].opt_bin}/python3 #{libexec}/pdfclassify.pyz "$@"
+	EOS
   end
   test do
 	  system "#{bin}/pdfclassify", "--version"
