b6655a9d1f7ab232babd110d93102398e3a2bed0
David Lane <git2018@dmlane.net>
2025-05-05 19:17:00 +0100

    Latest

--- Formula/pdfclassify.rb
+++ Formula/pdfclassify.rb
@@ -1,18 +1,19 @@
 class Pdfclassify < Formula
+  PYTHON_VERSION = "3.12"
   desc "Awesome CLI tool"
   homepage "https://github.com/dmlane/pdfclassify"
   url "https://github.com/dmlane/pdfclassify/releases/download/v1.0.28/pdfclassify-1.0.28.pyz"
   sha256 "0cc0daf087539cf49c3f32763f4357d338c4744056a651bc839e31a98f918530"
   license "MIT"
 
-  depends_on "python@3.12"
+  depends_on "python@#{PYTHON_VERSION}"
 
   def install
 	pyz = Dir["*.pyz"].first
 	libexec.install pyz
 	(bin/"pdfclassify").write <<~EOS
 		#!/bin/bash
-		exec #{Formula["python@3.12"].opt_bin}/python3.12 #{libexec}/#{pyz} "$@"
+		exec #{Formula["python@#{PYTHON_VERSION}"].opt_bin}/python#{PYTHON_VERSION} #{libexec}/#{pyz} "$@"
 	EOS
 	(bin/"pdfclassify").chmod 0755
   end
