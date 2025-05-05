d5d55581246f7966d7b72f6461f91a47a28f0cea
David Lane <git2018@dmlane.net>
2025-05-05 17:42:34 +0100

    Latest

--- Formula/pdfclassify.rb
+++ Formula/pdfclassify.rb
@@ -1,13 +1,17 @@
 class Pdfclassify < Formula
   desc "Awesome CLI tool"
   homepage "https://github.com/dmlane/pdfclassify"
-  url "https://github.com/dmlane/pdfclassify/releases/download/v1.0.19/pdfclassify-1.0.19.pyz"
-  sha256 "9083e261065918bf7d463eb7ad73c2f33d2a8b5b51c3d759b8e5a560b7cd3adb"
+  url "https://github.com/dmlane/pdfclassify/releases/download/v1.0.13/pdfclassify-1.0.13.pyz"
+  sha256 "246abf3cc3467cc9f0d9cba1b923ee11d3bcf2af6b2bac07be46d15e0be8d4e6"
   license "MIT"
 
   depends_on "python@3.12"
 
   def install
-    bin.install "pdfclassify-#{version}.pyz" => "pdfclassify"
+    #bin.install "pdfclassify-#{version}.pyz" => "pdfclassify"
+	virtualenv_install_with_resources
+  end
+  test do
+	  system "#{bin}/pdfclassify", "--version"
   end
 end
